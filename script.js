610b87177af969e78dc7cd4f6a6bec8517c801b5
prudhvigana <122306094+prudhvigana@users.noreply.github.com>
2023-03-27 20:52:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function func(){
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve("Hello, world!");
+		}, 1000);
+	});
+}
+func().then((value) => document.getElementById("output").innerText = value);
